5c8a835669d4ce2deb218d4bde6c15611c77dacd
RusMinin <ruslanminin2017@gmail.com>
2022-10-08 22:44:02 +0300

    новые функции

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-//qw
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function truncate(str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
+}
+//https://learn.javascript.ru/task/truncate
