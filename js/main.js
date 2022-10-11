9a81f05b20b9a6520d8416f1ac0b845082afbde2
Ruslan Minin <106439095+RusMinin@users.noreply.github.com>
2022-10-11 21:25:59 +0300

    Modul2 task1 (#2)
    * testing git
    
    * новые функции
    
    * Добавил Функций

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-//qw
+function getRandomIntInclusive(min, max) {
+  if(min<0 || max<0 || max<min){
+return NaN
+  }
+ min = Math.ceil(min);
+  max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function truncate(str, maxlength) {
+  return (str.length <=maxlength) ;
+}
+
+//https://learn.javascript.ru/task/truncate
